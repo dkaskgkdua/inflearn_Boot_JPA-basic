d2490657428085ff0a78f16bf2c815edf7b97296
SongMinJun <dkaskgkdua@gmail.com>
2021-01-30 14:35:22 +0900

    @매핑 정보 세팅

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -22,34 +22,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("Hello A");
-            em.persist(member);
-
-
-
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println(findMember.getId());
-            System.out.println(findMember.getName());
-
-            findMember.setName("Hello B");
-
-            Member findMember2 = em.find(Member.class, 1L);
-            System.out.println(findMember2.getName());
-            /**
-             * JPQL이라는 객체 지향 쿼리 언어 제공함
-             * 엔티티 객체를 대상으로 쿼리 구성
-             */
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)  // 페이징 시작
-                    .setMaxResults(5)   // 페이징 끝
-                    .getResultList();
-            for (Member item : result) {
-                System.out.println("member : " + item.getName());
-            }
-
-            em.remove(findMember2);
 
             tx.commit();
         } catch(Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,9 +1,9 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "USER")
@@ -11,22 +11,37 @@ public class Member {
     @Id
     private Long id;
 
-    @Column(name = "username")
+    @Column(name = "name", insertable = true, updatable = true
+            , nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
     private String name;
 
-    public Long getId() {
-        return id;
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    /**
+     * 시간 관련된 매핑 정보 세팅
+     * Date : 날짜
+     * Time : 시간
+     * TIMESTAMP : 날짜시간
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    /**
+     * 어노테이션 없어도 됨됨
+    */
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String description;
+
+    public Member() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
